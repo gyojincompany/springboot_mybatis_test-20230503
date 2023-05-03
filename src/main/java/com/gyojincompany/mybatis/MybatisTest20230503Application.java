865104f4be0bde3fcb266c719c06e50d82138d5d
ICI <ICI@DESKTOP-EA5M9P6>
2023-05-03 16:59:57 +0900

    mybatis insert 예제

---
+++ src/main/java/com/gyojincompany/mybatis/MybatisTest20230503Application.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisTest20230503Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisTest20230503Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyojincompany/mybatis/MybatisTest20230503ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisTest20230503ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
